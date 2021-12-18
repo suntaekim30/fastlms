16ff467c01381f5e0523dd93b102aaf63a8baaf7
suntae <suntaekim30@gmail.com>
2021-12-18 19:46:10 +0900

    fastlms 초기화 설정-2

--- fastlms/src/main/java/com/zorobase/fastlms/ServletInitializer.java
+++ fastlms/src/main/java/com/zorobase/fastlms/ServletInitializer.java
@@ -5,6 +5,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
+    //2021.12.18
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(FastlmsApplication.class);
